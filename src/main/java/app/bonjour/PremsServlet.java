508eb9f6f77c8a96f07b6588d766f7fa0f510831
Maxime TP <maxime.thoumin@protonmail.com>
2025-01-13 10:53:42 +0100

    Compteur de requêtes sur le premier servlet

--- src/main/java/app/bonjour/PremsServlet.java
+++ src/main/java/app/bonjour/PremsServlet.java
@@ -2,22 +2,32 @@ package app.bonjour;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serial;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 @WebServlet(urlPatterns = "/prems/*", loadOnStartup = 1)
 public class PremsServlet extends HttpServlet   {
+
+    int compteur = 0; // Compteur de requêtes
+
+    @Serial
     private static final long serialVersionUID = 1L;
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
         doGet(request,response);
+        compteur++;
+        System.out.println(compteur);
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("Hello chers ZZ3!");
+        compteur++;
+        out.println(compteur);
     }
 }
